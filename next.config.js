0fcf61fddf9055afcfc594841468420ccbfe7b82
berluche prunier <bpmac@MacBook-Air.local>
2023-10-23 04:31:15 -0400

    third commit pt3

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
     typescript: {
       // Warning: This allows production builds to successfully complete even if
       // your project has ESLint errors.
-      ignoreDuringBuilds: true,
+      ignoreBuildErrors: true,
     },
     images: {
       remotePatterns: [
